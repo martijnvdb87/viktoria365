0bfd68153b83123d55ad09045ebb154b8271b61c
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2022-07-19 16:42:15 +0200

    Added aside

--- src/components/Link.vue
+++ src/components/Link.vue
@@ -6,27 +6,27 @@ import Icon from '@/components/Icon.vue';
 const props = withDefaults(defineProps<{
   href: string,
   target?: string,
+  variant?: string,
 }>(), {
-  target: ''
+  target: '',
+  variant: 'default',
 });
 
 const isInternalLink = computed(() => {
-  const pattern = /^((http|https|ftp):\/\/)/;
+  const pattern = /^((http|https|ftp):\/\/)|mailto/;
   return !pattern.test(props.href);
 });
-
-
 </script>
 
 <template>
-  <router-link v-if="isInternalLink" :to="props.href" class="link">
+  <router-link v-if="isInternalLink" :to="props.href" :class="['link', `link--variant-${props.variant}`]">
     <span class="link__text">
       <slot></slot>
     </span>
     <Icon v-if="props.target === '_blank'" type="external-link" class="link__icon"></Icon>
   </router-link>
 
-  <a v-else :href="props.href" :target="props.target" class="link">
+  <a v-else :href="props.href" :target="props.target" :class="['link', `link--variant-${props.variant}`]">
     <span class="link__text">
       <slot></slot>
     </span>
@@ -36,13 +36,13 @@ const isInternalLink = computed(() => {
 
 <style scoped lang="scss">
 .link {
-  display: inline-flex;
+  display: inline-block;
   padding: 0 0.0625rem;
   color: var(--gray-700);
   font-weight: 450;
-  text-decoration: none;
-  align-items: center;
-  gap: 0.25rem;
+  text-decoration-color: var(--blue-350);
+  text-decoration-thickness: 0.125rem;
+  text-underline-offset: 0.125rem;
   border-radius: 0.125rem;
   transition: transform ease 160ms, color ease 160ms;
 
@@ -63,16 +63,6 @@ const isInternalLink = computed(() => {
 .link__text {
   isolation: isolate;
   position: relative;
-  
-  &::before {
-    position: absolute;
-    z-index: -1;
-    inset: 0 0 0.125rem 0;
-    content: '';
-    border-bottom: 0.125rem solid var(--blue-300);
-    pointer-events: none;
-    transition: ease all 160ms;
-  }
 }
 
 .link__icon {
@@ -81,5 +71,16 @@ const isInternalLink = computed(() => {
   height: 1rem;
   width: 0.875rem;
   height: 0.875rem;
+  margin-left: 0.25rem;
+  vertical-align: middle;
+}
+
+.link.link--variant-simple {
+  text-decoration-thickness: 0.0625rem;
+  color: var(--gray-650);
+
+  &:hover {
+    color: var(--blue-500);
+  }
 }
 </style>
--- src/components/Segment.vue
+++ src/components/Segment.vue
@@ -1,42 +1,53 @@
 <script setup lang="ts">
 import Icon from '@/components/Icon.vue';
 
-const props = defineProps<{
+const props = withDefaults(defineProps<{
   title?: string,
   icon?: string,
-}>()
+  format?: string,
+}>(), {
+  format: 'medium'
+});
 </script>
 
 <template>
-  <section>
-    <header v-if="props.title || props.icon">
-      <Icon v-if="props.icon" :type="props.icon" />
-      <h2 v-if="props.title">{{props.title}}</h2>
+  <section :class="['segment', `segment--${props.format}`]">
+    <header v-if="props.title || props.icon" class="segment__header">
+      <Icon v-if="props.icon" :type="props.icon" class="segment__icon" />
+      <h2 v-if="props.title" class="segment__title">{{props.title}}</h2>
     </header>
-    <div><slot></slot></div>
+    <div class="segment__content"><slot></slot></div>
   </section>
 </template>
 
 <style scoped lang="scss">
-section {
+.segment {
   margin-bottom: 4rem;
 }
 
-header {
+.segment__header {
   display: flex;
   gap: 0.625rem;
   align-items: center;
   margin-bottom: 0.5rem;
   color: var(--gray-900);
+}
 
-  svg {
-    width: 1.25rem;
-    height: 1.25rem;
-  }
+.segment__icon {
+  width: 1.25rem;
+  height: 1.25rem;
+}
 
-  h2 {
+.segment__title {
+  font-size: 0.9375rem;
+  font-weight: 600;
+}
+
+.segment.segment--small {
+  margin-bottom: 3rem;
+
+  .segment__content {
     font-size: 0.9375rem;
-    font-weight: 600;
   }
 }
 </style>
