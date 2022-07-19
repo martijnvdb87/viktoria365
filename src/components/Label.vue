18a08b825500402605c93a13a80328ae9d8bfd06
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2022-07-19 11:11:41 +0200

    Added Label

---
+++ src/components/Label.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <span class="label"><slot></slot></span>
+</template>
+
+<style scoped lang="scss">
+.label {
+  display: inline-block;
+  padding: 0.25rem 0.75rem;
+  font-size: 0.8125rem;
+  font-weight: 500;
+  color: var(--gray-600);
+  background-color: var(--gray-50);
+  border-radius: 999rem;
+}
+</style>
