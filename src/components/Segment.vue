b77b2a5db1266f469fd486a191a6913055be5a69
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2022-07-19 11:11:55 +0200

    Added Segment

---
+++ src/components/Segment.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import Icon from '@/components/Icon.vue';
+
+const props = defineProps<{
+  title?: string,
+  icon?: string,
+}>()
+</script>
+
+<template>
+  <section>
+    <header v-if="props.title || props.icon">
+      <Icon v-if="props.icon" :type="props.icon" />
+      <h2 v-if="props.title">{{props.title}}</h2>
+    </header>
+    <div><slot></slot></div>
+  </section>
+</template>
+
+<style scoped lang="scss">
+section {
+  margin-bottom: 4rem;
+}
+
+header {
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
+  font-size: 0.9375rem;
+  margin-bottom: 0.5rem;
+  color: var(--gray-700);
+
+  svg {
+    width: 1.25rem;
+    height: 1.25rem;
+  }
+
+  h2 {
+    font-size: 0.9375rem;
+    font-weight: 600;
+  }
+}
+</style>
