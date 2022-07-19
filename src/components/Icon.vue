f3913ca4de7fa5af3348198f294557dd90c1ed9a
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2022-07-19 11:11:28 +0200

    Added Icon

---
+++ src/components/Icon.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { computed, } from 'vue';
+
+import iconBriefcase from '@/assets/icons/briefcase.svg?raw';
+import iconCode from '@/assets/icons/code.svg?raw';
+import iconSmile from '@/assets/icons/smile.svg?raw';
+import iconUser from '@/assets/icons/user.svg?raw';
+
+const props = defineProps<{
+  type: string
+}>();
+
+const icon = computed(() => {
+  switch(props.type) {
+    case 'briefcase': return iconBriefcase;
+    case 'code': return iconCode;
+    case 'smile': return iconSmile;
+    case 'user': return iconUser;
+  }
+
+  return '';
+});
+
+
+</script>
+
+<template>
+  <svg v-if="icon" v-html="icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"></svg>
+</template>
+
+<style scoped>
+svg {
+  width: 1.5rem;
+  height: 1.5rem;
+  fill: none;
+  stroke: currentColor;
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+}
+</style>
