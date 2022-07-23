dee3b51b60d40363341b7134a3ce3188ca41405d
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2022-07-24 00:55:32 +0200

    Reworking LabelCloud

--- src/components/LabelCloud.vue
+++ src/components/LabelCloud.vue
@@ -1,9 +1,53 @@
 <script setup lang="ts">
+import Label from '@/components/Label.vue';
+
+const props = withDefaults(defineProps<{
+  showAll?: boolean,
+  labels?: string[]
+}>(), {
+  labels: () => []
+});
+
+const labels = [
+  'PHP',
+  'Javascript',
+  'Typescript',
+  'C# .NET',
+  'SQL',
+  'MongoDB',
+  'HTML',
+  'CSS',
+  'UI/UX',
+  'Laravel',
+  'Composer',
+  'React',
+  'Vue',
+  'Svelte',
+  'Web Components',
+  'Node.js',
+  'REST API',
+  'Git',
+  'SASS',
+  'JSON',
+  'OpenID Connect',
+  'SAML',
+  'OAuth',
+  'OAuth 2.0',
+  'PWA',
+  'Wordpress',
+  'Moodle',
+];
+
+const validLabel = (label: string): boolean => {
+  return props.labels.map(label => label.toLowerCase()).includes(label.toLowerCase());
+}
 </script>
 
 <template>
   <div class="label-cloud">
-    <slot></slot>
+    <template v-for="label in labels">
+      <Label v-if="validLabel(label) || props.showAll">{{label}}</Label>
+    </template>
   </div>
 </template>
 
